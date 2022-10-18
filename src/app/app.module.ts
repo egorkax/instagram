af3ce5fbaa1c9dcba1c1b31e19610a5c5b1ff432
EgorKokhnenko <kax.egor@mail.ru>
2022-10-18 22:24:00 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {Test1Component} from "./test1/test1.component";
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
+import { Test5Component } from './components/user/test5/test5.component';
+import { Test6Component } from './test6/test6.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component,
+    Test5Component,
+    Test6Component
   ],
   imports: [
     BrowserModule
